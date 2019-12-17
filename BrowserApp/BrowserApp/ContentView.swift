c2b1da6efdcd6d490d57d4d9f4047a92d0a6fc24
shin52 <b1018088@fun.ac.jp>
2019-12-17 19:42:39 +0900

    make xcode project

---
+++ BrowserApp/BrowserApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrowserApp
+//
+//  Created by Itou Shingo on 2019/12/17.
+//  Copyright © 2019 Itou Shingo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
