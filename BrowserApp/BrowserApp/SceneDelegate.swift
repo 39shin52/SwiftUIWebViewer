c4dea389a646178c1b19b5308c1ee48389f9eb04
shin52 <b1018088@fun.ac.jp>
2019-12-19 22:21:10 +0900

    ログイン(仮)画面つけた

--- BrowserApp/BrowserApp/SceneDelegate.swift
+++ BrowserApp/BrowserApp/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
         // Create the SwiftUI view that provides the window contents.
-        let contentView = firstView(url: URL(string: "https://google.com")!)
+        let contentView = firstView()
         
         //let contentView = ContentView()
         
